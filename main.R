d7564deb14b17bfcf9a008848be913d127baf575
agouy <contact@agouy.com>
2023-09-05 09:10:40 +0000

    First working version

--- main.R
+++ main.R
@@ -16,12 +16,13 @@ fs <- flowSet(ff)
 pars <- flowVS::estParamFlowVS(fs, channels = rn$label)
 fs_trans <- flowVS::transFlowVS(fs, rn$label, pars)
 
-apply(exprs(fs_trans[[1]]), 2, hist)
+df_out <- exprs(fs_trans[[1]])
+colnames(df_out) <- seq_len(ncol(df_out)) - 1L
 
-(fs_trans$`<B515-A> B515-A`)
-
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(mean = mean(.y)) %>%
+df_out %>% 
+  as_tibble() %>%
+  mutate(.ci = seq_len(nrow(.)) - 1L) %>%
+  tidyr::pivot_longer(!matches(".ci"), names_to = ".ri") %>%
+  mutate(.ri = as.integer(.ri)) %>%
   ctx$addNamespace() %>%
   ctx$save()
